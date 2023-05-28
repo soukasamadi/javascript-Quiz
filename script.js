75f47ba5500aac9368d80713fa31acc9b9b630d7
soukasamadi <>
2023-05-28 15:22:12 +0000

    create questions Array in javascript file

---
+++ script.js
@@ -0,0 +1,60 @@
+//questions, answers and options Array 
+
+let questions = [
+    {
+        number: 1,
+        question: "Inside which HTML element do we put the JavaScript?",
+        answer: "<script>",
+        options: [
+            "<js>>",
+            "<scripting>",
+            "<Head>",
+            "<script>"
+        ]
+    },
+    {
+        number: 2,
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answer: "<script src='xxx.js'>",
+        options: [
+            "<script href='xxx.js'>",
+            "<script name='xxx.js'>",
+            "<script src='xxx.js'>",
+            "<script type='xxx.js'>"
+        ]
+    },
+    {
+        number: 3,
+        question: "How do you write 'Hello World' in an alert box?",
+        answer: "alert('Hello World!')",
+        options: [
+            "alert('Hello World!')",
+            "alertBox('Hello World!')",
+            "print('Hello World!')",
+            "msgBox('Hello World!')"
+        ]
+    },
+    {
+        number: 4,
+        question: "How do you create a function in JavaScript?",
+        answer: "function myFunction()",
+        options: [
+            "function:myFunction()",
+            "function myFunction()",
+            "function = myFunction()",
+            "function == myFunction()"
+        ]
+    },
+    {
+        number: 5,
+        question: "How to write an IF statement in JavaScript?",
+        answer: "if (i == 5){}",
+        options: [
+            "if i = 5",
+            "if i = 5 then",
+            "if i == 5 then",
+            "if (i == 5){}"
+        ]
+    },
+
+];
